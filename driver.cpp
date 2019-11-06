8bd7f0602cb0bf7d79383a8263aacab2c8f36c51
Oleg Tielushko <tielushko@mail.usf.edu>
2019-11-05 20:15:13 -0500

    Test the coding/encoding from TestStrings.hpp Modify it to work as you wish

--- driver.cpp
+++ driver.cpp
@@ -3,14 +3,80 @@
 
 int main() {
     HuffmanTree t;
-    const std::string turing = "if a machine is expected to be infallible it cannot also be intelligent";
+    std::string compressed, serialized, decompress;
+    /*
+    //turing compress test
+    std::string compressed = t.compress(turing);
+    std::cout << compressed << std::endl;
+     if (turingCode == compressed) 
+        std::cout << "Yay" << std::endl;
     
-    std::string output = t.compress(turing);
-    if (turingCode == output) 
-        //std::cout << "Yay";
+    //turing serialized test
+    std::string serialized =  t.serializeTree();
+    std::cout << serialized << std::endl;
+    if (turingTree == serialized)
+        std::cout << "yay" << std::endl;
+ 
+    //turing decompress test
+    std::string decompress = t.decompress(compressed, serialized);
+    std::cout << decompress << std::endl;
+    if (turing == decompress)
+        std::cout << "yay" << std::endl;
+    
+    //dijkstra compress values
+    compressed = t.compress(dijkstra);
+    std::cout << compressed << std::endl;
+     if (dijkstraCode == compressed) 
+        std::cout << "Yay" << std::endl;
+    
+    //dijksta serialized test
+    serialized =  t.serializeTree();
+    std::cout << serialized << std::endl;
+    if (dijkstraTree == serialized)
+        std::cout << "yay" << std::endl;
+ 
+    //dijkstra decompress test
+    decompress = t.decompress(compressed, serialized);
+    std::cout << decompress << std::endl;
+    if (dijkstra == decompress)
+        std::cout << "yay" << std::endl;
 
-    std::cout << t.serializeTree();
-
+    //wikipedia compress values
+    compressed = t.compress(wikipedia);
+    std::cout << compressed << std::endl;
+     if (wikipediaCode == compressed) 
+        std::cout << "Yay" << std::endl;
+    
+    //wikipedia serialized test
+    serialized = t.serializeTree();
+    std::cout << serialized << std::endl;
+    if (wikipediaTree == serialized)
+        std::cout << "yay" << std::endl;
+ 
+    //wikipedia decompress test
+    decompress = t.decompress(compressed, serialized);
+    std::cout << decompress << std::endl;
+    if (wikipedia == decompress)
+        std::cout << "yay" << std::endl;
+    */
+    
+    //constitution compress values
+    compressed = t.compress(constitution);
+    std::cout << compressed << std::endl;
+     if (constitutionCode == compressed) 
+        std::cout << "Yay" << std::endl;
+    
+    //constitution serialized test
+    serialized = t.serializeTree();
+    std::cout << serialized << std::endl;
+    if (constitutionTree == serialized)
+        std::cout << "yay" << std::endl;
+ 
+    //constitution decompress test
+    decompress = t.decompress(compressed, serialized);
+    std::cout << decompress << std::endl;
+    if (constitution == decompress)
+        std::cout << "yay" << std::endl;
     
     return 0;
 }
